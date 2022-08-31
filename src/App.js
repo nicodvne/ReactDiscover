dac2a1e1a68a8e82a1403e43c2709d74b89f9866
Nicolas Davenne <nico.dvne@gmail.com>
2022-09-01 00:07:37 +0200

    16: Mise a jour du state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import Item from "./Item";
 import {useState} from 'react';
 
 function App() {
 
-  /*
-    si console.log(useState(10)) => tableau : 
-    [
-      0: 10,
-      1: f()... 
-    ]
-  */
-
   const [monState, setMonState] = useState(10);
-  console.log(monState); // useState(10)[0] => 10
-  console.log(setMonState); //useState(10)[1] => f()....
-  
+
+  const modifyState = () => {
+    setMonState(monState + 1);
+  }
 
   return (
     <div className="App">
-      <h1>Hello app react</h1>
-      <Item />
-      {monState} 
+      <h1>Hello state : {monState} </h1>
+    
+      <button onClick={modifyState}> Change state</button>
     </div>
   );
 }
