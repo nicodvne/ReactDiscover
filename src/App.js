1b4d441d7cf4c52b873758e99ee71bcb1e25c3cb
Nicolas Davenne <nico.dvne@gmail.com>
2022-09-08 00:08:11 +0200

    19 : Utiliser du css avec react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import {useState} from 'react';
-import Item from './Item';
+import Item from './Components/Item/Item';
 
 function App() {
 
-  const [monState, setMonState] = useState(10);
-
-  /* Etape 3 : je prend le state de Item et je le set dans le state de App */
-  const modifyState = (data) => {
-    setMonState(data);
-  }
-
   return (
     <div className="App">
-      <h1>Hello state : {monState} </h1>
-          {/* 
-            Etape 1 : Je passe la fonction de modif de state 
-            en props dans le composant fils 
-          */}
-          <Item func={modifyState}/>
+      <h1>Hello depuis App.js</h1>
+      <Item/>
     </div>
 
   );
