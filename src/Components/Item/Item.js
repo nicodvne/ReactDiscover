0cd51ba472229a16fdf1445954b25eadc4be4f26
Nicolas Davenne <nico.dvne@gmail.com>
2022-09-15 20:50:15 +0200

    alter components in folder

---
+++ src/Components/Item/Item.js
@@ -0,0 +1,11 @@
+import './Item.css'
+
+function Item (props) {
+    return (
+        <div>
+            <h2 className="title-item">Hello depuis Item</h2>
+        </div>
+    )
+}
+
+export default Item
