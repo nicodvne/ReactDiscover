d4126aa09c7ce38382ad73facb5fd94420b09fcb
Nicolas Davenne <nico.dvne@gmail.com>
2022-09-15 21:08:59 +0200

    20 - Utiliser des images

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,9 +1,13 @@
 import './Item.css'
+import imgItem from '../../assets/imgItem.jpg'
 
 function Item (props) {
     return (
         <div>
             <h2 className="title-item">Hello depuis Item</h2>
+            <img 
+                src={imgItem} alt="une image random"
+            />
         </div>
     )
 }
