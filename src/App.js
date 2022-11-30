f538ac2abe41f560ae421d5128b2780cd3c2fbee
Nicolas Davenne <nico.dvne@gmail.com>
2022-11-30 18:30:27 +0100

    Base React ok

--- src/App.js
+++ src/App.js
@@ -3,22 +3,40 @@ import Item from './Components/Item/Item';
 
 function App() {
 
-  const [toogle, setToogle] = useState(true);
+  const [dataArr, setDataArr] = useState([
+    {nom: "Juliette"},
+    {nom: "Nicolas"},
+    {nom: "John"},
+  ]);
 
-  const changeState = () => {
+  /**
+   * const changeState = () => {
     setToogle(!toogle);
   }
+   */
+  
 
-  let toogleContent;
-  toogleContent = toogle ? <h1>Le state est true</h1> : <h1>Le state est false</h1>
-
-    return (
+  /**
+   * return (
       <div className="App">
-        {toogleContent}
+        <div className={toogle ? "box animated" : "box"}></div>
         <button onClick={changeState}>Change State</button>
       </div>
   
     );
+   */
+
+    return (
+      <div className="App">
+         {/**
+          * Ici, l'index nous permet d'indentifier de facon unique (0, 1, 2) les éléments de la liste comme les index d'un tableau
+          * Il s'agit d'un comportement de base de la méthode map
+          * Si pas d'index -> erreur dans la console JS
+          */}
+          { dataArr.map((item, index) => { return <p key={index}> Nom: {item.nom}</p> })}
+      </div>
+    )
+    
   
    
   
